c5081710f8220e1211ee30a02a744ac2c5086987
ashaduzzamane <enan@live.ca>
2020-05-29 16:01:11 -0400

    Updates

---
+++ precache-manifest.080a141307e77f16a4f03f06ff51ff9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f660bfe23c7fc0ef8e6fe4aebb15e6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2fe241f04a595f5c34f",
+    "url": "/portfolio/static/css/main.fff0300a.chunk.css"
+  },
+  {
+    "revision": "c85c2f912607c02fb3f1",
+    "url": "/portfolio/static/js/2.3c67d267.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/portfolio/static/js/2.3c67d267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fe241f04a595f5c34f",
+    "url": "/portfolio/static/js/main.8f292336.chunk.js"
+  },
+  {
+    "revision": "ae96be6337f62866a6b5",
+    "url": "/portfolio/static/js/runtime-main.311230cf.js"
+  }
+]);
