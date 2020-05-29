24afb4408363bbde8005d2561caca155b034ce73
ashaduzzamane <enan@live.ca>
2020-05-29 16:45:52 -0400

    Updates

--- precache-manifest.c3843b568b424329813ecfc9d240cb9e.js
+++ precache-manifest.c3843b568b424329813ecfc9d240cb9e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53d31a7db900551e2d81a79f53f248cf",
+    "revision": "c8154eea1e0fd742164e8819c74e9a5a",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "734fa2d27e63e1bcb9a4",
+    "revision": "f68d707741fad7a996d0",
     "url": "/portfolio/static/css/main.fff0300a.chunk.css"
   },
   {
-    "revision": "d4b93a93c451596cffc8",
-    "url": "/portfolio/static/js/2.6d14e5fb.chunk.js"
+    "revision": "ced5740a7c8735356394",
+    "url": "/portfolio/static/js/2.84e863b7.chunk.js"
   },
   {
     "revision": "44e39e61fb6a0450f7c8014318774752",
-    "url": "/portfolio/static/js/2.6d14e5fb.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.84e863b7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "734fa2d27e63e1bcb9a4",
-    "url": "/portfolio/static/js/main.dc263232.chunk.js"
+    "revision": "f68d707741fad7a996d0",
+    "url": "/portfolio/static/js/main.c38e7514.chunk.js"
   },
   {
     "revision": "ae96be6337f62866a6b5",
